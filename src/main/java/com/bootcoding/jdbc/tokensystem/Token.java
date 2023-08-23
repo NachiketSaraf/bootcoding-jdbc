e9f9da0301c5d3a5c1ef610092180ae0d223f869
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-23 23:47:35 +0530

    Token

---
+++ src/main/java/com/bootcoding/jdbc/tokensystem/Token.java
@@ -0,0 +1,10 @@
+package com.bootcoding.jdbc.tokensystem;
+
+import java.util.Date;
+
+public class Token {
+    private int id;
+    private int tokenNo;
+    private String counterNo;
+    private Date dateTime;
+}
