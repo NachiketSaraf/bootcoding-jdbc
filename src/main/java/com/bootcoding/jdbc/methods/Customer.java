13f42b0f6b625b79e07abc05df56b3dbd45997d7
NachiketSaraf <sarafnachiket@gmail.com>
2023-08-23 23:44:28 +0530

    Customer

---
+++ src/main/java/com/bootcoding/jdbc/methods/Customer.java
@@ -0,0 +1,49 @@
+package com.bootcoding.jdbc.methods;
+
+public class Customer {
+    private int customer_id;
+    private String cust_name;
+    private String city;
+    private String email;
+    private int salesmanId;
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCust_name() {
+        return cust_name;
+    }
+
+    public void setCust_name(String cust_name) {
+        this.cust_name = cust_name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSalesmanId() {
+        return salesmanId;
+    }
+
+    public void setSalesmanId(int salesmanId) {
+        this.salesmanId = salesmanId;
+    }
+}
